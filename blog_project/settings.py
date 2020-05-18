e4333f479b4f8db60abbabf170051a28610cd537
desittisagar <dsagar.smart@gmail.com>
2020-05-19 01:06:56 +0530

    Heroku config files and updates

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'pinr5n74=_e3z(#thlfv^izt2gg6)xkzlzu8bu=i)n*o8i^q9f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
